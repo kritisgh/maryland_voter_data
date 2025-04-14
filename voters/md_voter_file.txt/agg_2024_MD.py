76a7fc9c035d34a8b90495d9a598ee1fc397b964
rushabhkamdar44 <rkamdar2@terpmail.umd.edu>
2025-04-14 20:43:35 +0000

    cleaned up dataset

--- voters/md_voter_file.txt/agg_2024_MD.py
+++ voters/md_voter_file.txt/agg_2024_MD.py
@@ -1,9 +1,12 @@
 import pandas as pd
 
-df = pd.read_csv('agg_MD.csv')
+df = pd.read_csv('agg_MD.csv', index_col=0)
 
-df_No_2020 =df.drop('voted_2020', axis=1)
-df.drop('voted_2016', axis=1)
+df_No_2020 = df.drop('voted_2020', axis='columns', inplace =True)
+df_No_2016 = df.drop('voted_2016', axis='columns', inplace =True)
+df_No_gender = df.drop('Gender', axis='columns', inplace =True)
+
+df.to_csv('2024_agg_md.csv', index=False)
 
 
-print(df_No_2020)
+
